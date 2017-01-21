76723be4ea3ef9a4ca9914cb0e85175d9cfb9ba4
Benedikt Zoeller <sephirot5@web.de>
2017-01-21 01:05:02 +0100

    Added SeeableSpheres

---
+++ GlobalGamejam2017/Assets/Scripts/LightSignal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSignal : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0.01f,1)]
+    private float startIntensity = 1;
+
+    [SerializeField]
+    [Range(0.0001f, 0.1f)]
+    private float lossPerOverTime = 0.01f;
+
+    [SerializeField]
+    [Range(0.0001f, 0.1f)]
+    private float growPerOverTime = 0.01f;
+
+    [SerializeField]
+    [Range(0, 200)]
+    public float maxDepth = 0;
+
+    private float startSize;
+    public float intensity;
+
+    private bool stopIncreasing = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        startSize = transform.localScale.x;
+        Reset();
+        Debug.Log(stopIncreasing);
+    }
+
+    public bool GetStopIncreasing()
+    {
+        return stopIncreasing;
+    }
+
+    private void Reset()
+    {
+        stopIncreasing = false;
+        transform.localScale = new Vector3(startSize, startSize, startSize);
+        intensity = startIntensity;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!stopIncreasing)
+            UpdateGrow();
+        if (intensity <= 0)
+            stopIncreasing = true;
+	}
+
+    private void UpdateGrow()
+    {
+        intensity -= lossPerOverTime;
+        transform.localScale = transform.localScale * 1 / (1 - growPerOverTime);
+    }
+}
