68b4935954e27c6ae24b23f794fcc8f5a7ec8035
Benedikt Zoeller <sephirot5@web.de>
2017-01-21 19:58:03 +0100

    Interaktions-Skripte
    einfach erstellbar. Kleiner Bug: führt sich permanent aus, wenn in kollision mit anderem interagierbarem Objekt. sollte jedoch leicht vermeidbar sein.

--- GlobalGamejam2017/Assets/Scripts/PushableObject.cs
+++ GlobalGamejam2017/Assets/Scripts/PushableObject.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class PushableObject : MonoBehaviour {
 
+    GameObject soundWave;
+
 	// Use this for initialization
 	void Start () {
-		
+        soundWave = GameObject.FindGameObjectWithTag("SoundWave");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void interact()
+    {
+
+    }
 }
