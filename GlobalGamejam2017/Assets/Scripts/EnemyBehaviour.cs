5f18de9580a5ca999b104b847740b5206e0f4e2f
Benedikt Zoeller <sephirot5@web.de>
2017-01-22 00:55:29 +0100

    Merge-Progress

--- GlobalGamejam2017/Assets/Scripts/EnemyBehaviour.cs
+++ GlobalGamejam2017/Assets/Scripts/EnemyBehaviour.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -53,31 +52,3 @@ public class EnemyBehaviour : MonoBehaviour {
         iFrames = iFrameCooldown;
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Audio;
-
-public class EnemyBehaviour : MonoBehaviour {
-    
-    Transform playerTransform;
-
-    [SerializeField]
-    float speed;
-    [SerializeField]
-    float distanceToPlayer;
-
-
-    private void Start()
-    {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
-    
-    
-	void Update ()
-    {
-        transform.LookAt(playerTransform);
-    }
-}
->>>>>>> master
