13305c7c7431a7a341d8866aab42f437674966b5
Benedikt Zoeller <sephirot5@web.de>
2017-01-22 08:24:28 +0100

    Working on Camera. Bug?

--- GlobalGamejam2017/Assets/Scripts/CameraScript.cs
+++ GlobalGamejam2017/Assets/Scripts/CameraScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class CameraScript : MonoBehaviour {
 
@@ -13,17 +14,26 @@ public class CameraScript : MonoBehaviour {
     // Use this for initialization
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
-        rigid = GetComponent<Rigidbody>();
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float desiredAngle = player.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        //transform.position = transform.position + (player.transform.position - (rotation * offSet) - transform.position) / 10;
+        Debug.Log((player.transform.position - (rotation * offSet) - transform.position) * 100);
+        rigid.velocity = new Vector3(1,1,1);
+        Debug.Log(rigid.velocity);
+        rigid.velocity = rigid.velocity / 2;
     }
 
     // Update is called once per frame
     void Update() {
-        float desiredAngle = player.transform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-        //transform.position = transform.position + (player.transform.position - (rotation * offSet) - transform.position) / 10;
-        rigid.AddForce((player.transform.position - (rotation * offSet) - transform.position) * 100);
-        rigid.velocity = rigid.velocity / 2;
 
+
+        //if (GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y > 0.2f || GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y < -0.2f)
+        //    offSet.y += GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.Y * 0.1f;
 
         transform.LookAt(player.transform);
     }
